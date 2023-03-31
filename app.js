c514f9600e4490a67b3b280baae03aced160f23d
yasinerel <terel19@ku.edu.tr>
2023-03-31 03:06:39 +0300

    getting better

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ function getWeather(event) {
             document.querySelector('.temp').textContent = `${data.current.temp_c}°C`;
             console.log(data.current.temp_c);
             document.querySelector('.desc').textContent = data.current.condition.text;
+            document.getElementById("location").innerHTML = city;
+
         })
         .catch(error => console.error(error));
 }
+
+
+function showDate() {
+    var today = new Date();
+    var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][today.getDay()];
+    var dayOfMonth = today.getDate();
+    var monthOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][today.getMonth()];
+    var year = today.getFullYear();
+    var currentDate = dayOfMonth + " " + monthOfYear + " " + year;
+    document.getElementById("currentDay").innerHTML = dayOfWeek;
+    document.getElementById("currentDate").innerHTML = dayOfMonth + " " + monthOfYear + ", " + year;
+
+
+
+
+  }
